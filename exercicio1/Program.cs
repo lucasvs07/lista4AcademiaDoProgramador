542a71e49775f425e0034b2ef8b689552d066451
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-15 12:29:52 -0300

    Add files via upload

---
+++ exercicio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Desenvolver um algoritmo que leia a altura de 15 pessoas. Este
+        programa deverá calcular e mostrar:
+        a. A menor altura do grupo;
+        b. A maior altura do grupo;*/
+
+        float[] alturas = new float[15];
+
+        float maior = alturas[0];
+        float menor = 1005775; //Isso aqui é a mais pura gambiarra.
+        int i = 0;
+
+        do
+        {
+            Console.WriteLine("Digite a altura:");
+            string straltura = Console.ReadLine();
+            alturas[i] = float.Parse(straltura);
+
+            if (alturas[i] <= menor)
+                menor = alturas[i];
+
+            if (alturas[i] > maior)
+                maior = alturas[i];
+
+            i++;
+        }
+
+
+        while (i < 15);
+        string strmenorfinal = Convert.ToString(menor);
+        string strmaiorfinal = Convert.ToString(maior);
+
+        Console.WriteLine("A pessoa com a menor altura do grupo tem " + strmenorfinal + " m.");
+        Console.WriteLine("A pessoa com a maior altura do grupo tem " + strmaiorfinal + " m.");
+        Console.ReadKey();
+
+    }
+}
