455469fedc3e0000c162d925347e9b2bcff44951
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-15 12:30:59 -0300

    Add files via upload

---
+++ exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex2L3LucasVinicius.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escrever um algoritmo que gera e escreve os números ímpares entre 100
+            e 200.*/
+            
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine(i + " é impar.");
+            }
+            Console.ReadKey();
+                
+        }
+    }
+}
